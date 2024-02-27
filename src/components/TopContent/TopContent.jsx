516cd548ddca9af2ae9283dda5acd1913084229b
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2024-02-27 12:46:34 +0100

    download cv button and my projects

--- src/components/TopContent/TopContent.jsx
+++ src/components/TopContent/TopContent.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const TopContent = () => {
   return (
@@ -7,6 +8,11 @@ const TopContent = () => {
       <div className='TopContent_container'>
         <h1>Tamizh ilakia Rajarathinam</h1>
         <p> Front-end Developer </p>
+        <a><button>Download CV</button></a>
+        <Link to = "projects">
+            <button>My Projects</button>
+        </Link>
+
       </div>
     </div>
   );
